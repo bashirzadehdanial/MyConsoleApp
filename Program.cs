3021c9c7f14e2f0376c9676f2027e76671b43af9
Danial <bashirzadehdanial@gmail.com>
2024-07-20 23:21:03 +0100

    Add main application logic in Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Program.cs
+using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Alice", 30);
+            person.Introduce();
+
+            Student student = new Student("Bob", 20, "University of Example");
+            student.Introduce();
+            student.Study();
+        }
+    }
+}
